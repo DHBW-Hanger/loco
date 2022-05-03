99b0cf04f6f351cd29be54d4191ad8af88e88ab4
weltitob <weltitob@gmail.com>
2022-05-03 14:25:46 +0200

    Message to all contributors: Create your own branch!

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+          HALLO FREUNDE!
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bitte alle einen eigenen Branch erstellen!
         </p>
         <a
           className="App-link"
