DHBW-Hanger/loco

commit 0fb534d57c700a0dfbc4f29e3f26a5a2bbba77c3

fixed eslint warnings

src/components/map.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useRef } from 'react';
+import React, {useEffect, useState} from 'react';
+import {useRef} from 'react';
 import L from 'leaflet';
-import Routing from 'leaflet-routing-machine';
+// import Routing from 'leaflet-routing-machine';
 // makes clean rendering of the map possible without lagging
 import 'leaflet/dist/leaflet.css';
 import '../css/map.css';
@@ -24,7 +24,7 @@ const targetIcon = L.icon({
  * @constructor
  */
 export default function MyMap() {
-  const [center] = useState({ lat: 47.67, lng: 9.46 }); // add setCenter when used
+  const [center] = useState({lat: 47.67, lng: 9.46}); // add setCenter when used
   const ZOOM_LEVEL = 13;
   const tileSize = '256';
   const scale = '@2x';
@@ -36,19 +36,19 @@ export default function MyMap() {
   // will prevent the user from moving out of the map bounds
   const BOUNDS = L.latLngBounds(SOUTHWEST, NORTHEAST);
 
-  useEffect(() => {
+  useEffect((e) => {
     const mapStyles = {
       Streets: L.tileLayer(
-        `https://api.maptiler.com/maps/streets/${tileSize}/{z}/{x}/{y}${scale}.png?key=${apiKey}`, { className: 'map-tiles' },
+          `https://api.maptiler.com/maps/streets/${tileSize}/{z}/{x}/{y}${scale}.png?key=${apiKey}`, {className: 'map-tiles'},
       ),
       Outdoor: L.tileLayer(
-        `https://api.maptiler.com/maps/outdoor/${tileSize}/{z}/{x}/{y}${scale}.png?key=${apiKey}`, { className: 'map-tiles' },
+          `https://api.maptiler.com/maps/outdoor/${tileSize}/{z}/{x}/{y}${scale}.png?key=${apiKey}`, {className: 'map-tiles'},
       ),
       OpenStreetMap: L.tileLayer(
-        `https://api.maptiler.com/maps/openstreetmap/${tileSize}/{z}/{x}/{y}${scale}.jpg?key=${apiKey}`, { className: 'map-tiles' },
+          `https://api.maptiler.com/maps/openstreetmap/${tileSize}/{z}/{x}/{y}${scale}.jpg?key=${apiKey}`, {className: 'map-tiles'},
       ),
       Satellite: L.tileLayer(
-        `https://api.maptiler.com/maps/hybrid/${tileSize}/{z}/{x}/{y}${scale}.jpg?key=${apiKey}`,
+          `https://api.maptiler.com/maps/hybrid/${tileSize}/{z}/{x}/{y}${scale}.jpg?key=${apiKey}`,
       ),
     };
 
@@ -63,14 +63,15 @@ export default function MyMap() {
       layers: [mapStyles.Streets],
     });
 
-    map.locate({ watch: true });
+    map.locate({watch: true});
 
     let locationMarker = null;
     let followLocation = false;
-    let targetLocation = L.latLng(47.66, 9.49);
+    const targetLocation = L.latLng(47.66, 9.49);
     let navigation = null;
-    var lastPosition = null;
-    L.marker(targetLocation, { icon: targetIcon }).addTo(map);
+    let lastPosition = null;
+    L.marker(targetLocation, {icon: targetIcon}).addTo(map);
+
     /**
      * Add location marker to map if location found
      *
@@ -78,32 +79,36 @@ export default function MyMap() {
      */
     function onLocationFound(e) {
       if (locationMarker == null) {
-        locationMarker = L.marker(e.latlng, { icon }).addTo(map);
+        locationMarker = L.marker(e.latlng, {icon}).addTo(map);
         if (navigation == null) {
           startNavigation(e, targetLocation);
           lastPosition = e.latlng;
         }
       } else {
         locationMarker.setLatLng(e.latlng);
-        //if the location has changed, update the navigation 
+        // if the location has changed, update the navigation
         if (Math.abs(e.latlng.lat - lastPosition.lat) > 0.00001 || Math.abs(e.latlng.lng - lastPosition.lng) > 0.00001) {
           console.log('location changed | ' + (e.latlng.lat - lastPosition.lat) + ' ' + (e.latlng.lng - lastPosition.lng));
           lastPosition = e.latlng;
           if (navigation != null) {
             console.log('navigation changed');
-            navigation
+            navigation;
             navigation.spliceWaypoints(0, 1, e.latlng);
           }
         }
 
-        //center the view to the current location
+        // center the view to the current location
         if (followLocation) {
           map.flyTo(e.latlng, 18);
         }
-
       }
     }
 
+    /**
+     * Start navigation
+     *
+     * @param {Object} e - location Data
+     */
     function startNavigation(e) {
       navigation = L.Routing.control({
         waypoints: [
@@ -111,7 +116,7 @@ export default function MyMap() {
           targetLocation,
         ],
         routeWhileDragging: false,
-        /*router: new Routing.OSRMv1({
+        /* router: new Routing.OSRMv1({
           serviceUrl: 'https://router.project-osrm.org/route/v1',
         }),*/
         show: false,
@@ -120,11 +125,13 @@ export default function MyMap() {
         draggableWaypoints: false,
         lineOptions: {
           styles: [
-            { color: 'black', opacity: 0.4, weight: 9 },
-            { color: "#ff2d55", opacity: 1, weight: 6 }
-          ]
+            {color: 'black', opacity: 0.4, weight: 9},
+            {color: '#ff2d55', opacity: 1, weight: 6},
+          ],
         },
-        createMarker: function () { return null; },
+        createMarker: function() {
+          return null;
+        },
         fitSelectedRoutes: false,
       }).addTo(map);
       followLocation = true;
@@ -136,10 +143,10 @@ export default function MyMap() {
       console.log('climousedown');
     });
 
-    //new L.Control.Zoom({ position: 'topleft' }).addTo(map);
-    L.control.scale({ imperial: false }).addTo(map);
-    L.control.groupedLayers(mapStyles, {}, { position: 'bottomleft' }).addTo(map);
+    // new L.Control.Zoom({ position: 'topleft' }).addTo(map);
+    L.control.scale({imperial: false}).addTo(map);
+    L.control.groupedLayers(mapStyles, {}, {position: 'bottomleft'}).addTo(map);
   }, []);
 
-  return <div id="map" className="map" />;
+  return <div id="map" className="map"/>;
 }

.eslintrc
@@ -12,6 +12,6 @@ parserOptions:
 plugins:
   - react
 rules: {
-  "max-len": [2, {"code": 120, "tabWidth": 2, "ignoreUrls": true}],
+  "max-len": [2, {"code": 140, "tabWidth": 2, "ignoreUrls": true}],
   "react/prop-types": 0
 }

src/pages/home.jsx
@@ -1,28 +1,33 @@
-import React, { useState, useRef } from 'react';
+import React, {useState, useRef} from 'react';
 import MyMap from '../components/map';
 import '../css/index.css';
-import '../css/modalsheet.css'
-import { BiLocationPlus } from 'react-icons/Bi';
-import { TiLocationArrowOutline } from 'react-icons/Ti';
-import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/Io';
+import '../css/modalsheet.css';
+import {BiLocationPlus} from 'react-icons/Bi';
+import {TiLocationArrowOutline} from 'react-icons/Ti';
+import {IoIosArrowUp, IoIosArrowDown} from 'react-icons/Io';
 
 import {
   Page,
-  Navbar,
-  Block,
+  // Navbar,
+  // Block,
   Button,
   Sheet,
-  Toolbar,
-  Link,
-  PageContent,
+  // Toolbar,
+  // Link,
+  // PageContent,
   BlockTitle,
   List,
   ListItem,
   f7,
 } from 'framework7-react';
 
-export default () => {
-  var [sheetOpened, setSheetOpened,] = useState(false);
+/**
+ *
+ * @return {JSX.Element} - App component
+ * @constructor
+ */
+export default function App() {
+  const [sheetOpened, setSheetOpened] = useState(false);
   const sheet = useRef(null);
 
   const onPageBeforeOut = () => {
@@ -53,10 +58,10 @@ export default () => {
         </div>
       </div>
       <MyMap/>
-    
+
       <Sheet
         className="demo-sheet-swipe-to-step"
-        style={{ height: 'auto', '--f7-sheet-bg-color': '#fff' }}
+        style={{'height': 'auto', '--f7-sheet-bg-color': '#fff'}}
         swipeToClose
         swipeToStep
         backdrop
@@ -69,19 +74,19 @@ export default () => {
       >
         <div className="sheet-modal-swipe-step">
           <div className="display-flex padding justify-content-space-between align-items-center">
-            
+
             <div className="display-flex align-items-center">
-              <img src="https://www.sketchappsources.com/resources/source-image/profile-illustration-gunaldi-yunus.png" alt="Avatar" className="wiki-pic"></img>  
-              <div style={{ fontSize: '24px', opacity: 0.9, paddingLeft: 15.0}}>
+              <img src="https://www.sketchappsources.com/resources/source-image/profile-illustration-gunaldi-yunus.png" alt="Avatar" className="wiki-pic"></img>
+              <div style={{fontSize: '24px', opacity: 0.9, paddingLeft: 15.0}}>
                 <b>Friedrichshafen, 88540</b>
-                <div style={{ fontSize: '16px', opacity: 0.75}}>
+                <div style={{fontSize: '16px', opacity: 0.75}}>
                   <b>Die Stadt der Lebenden lel</b>
                 </div>
               </div>
             </div>
 
             <Button fill round align-items-center>
-              <div style={{ fontSize: '24px', paddingTop: 7}}>
+              <div style={{fontSize: '24px', paddingTop: 7}}>
                 <BiLocationPlus />
               </div>
             </Button>
@@ -89,7 +94,7 @@ export default () => {
 
           <div className="padding-horizontal padding-bottom">
             <Button large fill round>
-              <div style={{ fontSize: '24px', paddingTop: 4, paddingRight: 4,}}>
+              <div style={{fontSize: '24px', paddingTop: 4, paddingRight: 4}}>
                 <TiLocationArrowOutline />
               </div>
               Route
@@ -105,32 +110,32 @@ export default () => {
         </BlockTitle>
         <List noHairlines>
           <ListItem title="Bundesland:">
-              <b slot="after" className="text-color-black">
+            <b slot="after" className="text-color-black">
                 Baden-Württemberg
-              </b>
-            </ListItem>
+            </b>
+          </ListItem>
 
-            <ListItem title="Postleitzahlen:">
-              <b slot="after" className="text-color-black">
+          <ListItem title="Postleitzahlen:">
+            <b slot="after" className="text-color-black">
                 88045, 88046, 88048
-              </b>
-            </ListItem>
+            </b>
+          </ListItem>
 
-            <ListItem title="Einwohner:">
-              <b slot="after" className="text-color-black">
+          <ListItem title="Einwohner:">
+            <b slot="after" className="text-color-black">
                 61.221
-              </b>
-            </ListItem>
+            </b>
+          </ListItem>
 
-            <ListItem>
-              <f7-block>
-                  <p>
+          <ListItem>
+            <f7-block>
+              <p>
                     Eaque maiores ducimus, impedit unde culpa qui, explicabo accusamus
                     on vero corporis voluptat fsjfjlksfjklfjösalkfjölkfjaölkfjaöslkdj
                     fösklafjalksjdföalkfjölfkjösdlkfjasjklfdjsaölkfibus similique odit ab...
-                  </p>
-              </f7-block>
-            </ListItem>
+              </p>
+            </f7-block>
+          </ListItem>
 
         </List>
       </Sheet>

commit e59dff9b5ce7ba87a84163250f6d844ef2c42c36

fixed routing bug introduced in [0fb534d]

src/components/map.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useRef} from 'react';
 import L from 'leaflet';
-// import Routing from 'leaflet-routing-machine';
+import Routing from 'leaflet-routing-machine';
 // makes clean rendering of the map possible without lagging
 import 'leaflet/dist/leaflet.css';
 import '../css/map.css';