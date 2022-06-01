0f2506a8077f78ba44a87e10f9e5d0af5dab567f
Andreas Holzapfel <holzapfel.a1994@gmail.com>
2022-06-01 20:56:16 +0200

    linter fix

--- src/js/wikipediaCall.js
+++ src/js/wikipediaCall.js
@@ -1,6 +1,12 @@
 const userInput = 'Friedrichshafen';
 
-
+/**
+ * wikipedia Api Call for everything
+ *
+ * @param longitude
+ * @param latitude
+ * @returns {Promise<void>}
+ */
 async function wikiCall(longitude = 9.44376, latitude = 47.667223) {
   await reverseGeocoding(longitude, latitude);
   // searchWiki(userInput);
@@ -8,7 +14,11 @@ async function wikiCall(longitude = 9.44376, latitude = 47.667223) {
   await getImages(userInput);
 
   /**
-   * gibt Adresse auf der Konsole aus
+   * prints address on console
+   *
+   * @param lon
+   * @param lat
+   * @returns {Promise<void>}
    */
   async function reverseGeocoding(lon, lat) {
     const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lon=${lon}&lat=${lat}`);
@@ -16,6 +26,11 @@ async function wikiCall(longitude = 9.44376, latitude = 47.667223) {
     console.log(data['display_name']);
   }
 
+  /**
+   * searches wiki with user input
+   *
+   * @param userInput
+   */
   function searchWiki(userInput) {
     const url = `https://de.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&formatversion=2&origin=*&srlimit=20&srsearch=${userInput}`;
     fetch(
@@ -34,6 +49,11 @@ async function wikiCall(longitude = 9.44376, latitude = 47.667223) {
         });
   }
 
+  /**
+   * prints the content of the wiki page
+   *
+   * @param userInput
+   */
   function contentWiki(userInput) {
     const url = `https://de.wikipedia.org/w/api.php?action=query&titles=${userInput}&prop=revisions&rvprop=content&format=json&formatversion=2&origin=*`;
     console.log(url);
@@ -53,15 +73,20 @@ async function wikiCall(longitude = 9.44376, latitude = 47.667223) {
         });
   }
 
+  /**
+   * prints urls of images on console
+   *
+   * @param location
+   * @returns nothing so far
+   */
   async function getImages(location) {
-    let imageNames;
-    let apiurls;
-    let contentImageApi;
-    let content = [];
     // get names of images
-    imageNames = await imageWikiUrls(location);
+    const imageNames = await imageWikiUrls(location);
     // create urls for image api Call
-    apiurls = await createImageAPIUrls(imageNames);
+    const apiurls = await createImageAPIUrls(imageNames);
+    let contentImageApi;
+    const content = [];
+
     // get image urls
     for (let i = 0; i < apiurls.length; i++) {
       contentImageApi = await getImageUrl(apiurls[i]);
@@ -70,14 +95,18 @@ async function wikiCall(longitude = 9.44376, latitude = 47.667223) {
           content.push(contentImageApi['query']['pages'][0]['imageinfo'][0]['url']);
         }
       } else {
-        content.push("");
+        content.push('');
       }
     }
-    console.log(content)
+    console.log(content);
   }
 
+  /**
+   * gets name of 30 images (imlimit=30)
+   * @param userInput
+   * @returns {Promise<*>}
+   */
   async function imageWikiUrls(userInput) {
-    //gets name for of 30 images (imlimit=30)
     const url = `https://de.wikipedia.org/w/api.php?action=query&titles=${userInput}&format=json&prop=images&imlimit=30&origin=*`;
     /*
     let url = "https://de.wikipedia.org/w/api.php?"
@@ -93,7 +122,7 @@ async function wikiCall(longitude = 9.44376, latitude = 47.667223) {
       url += "&" + key + "=" + params[key];
     });
     */
-   let content;
+    let content;
     await fetch(
         url,
         {
@@ -108,16 +137,18 @@ async function wikiCall(longitude = 9.44376, latitude = 47.667223) {
     return content;
   }
 
+  /**
+   * creates the url apicalls for the imagenames and only includes the .jpg files
+   * @param data
+   * @returns {Promise<*[]>}
+   */
   async function createImageAPIUrls(data) {
-    /*
-    creates the url apicalls for the imagenames and only includes the .jpg files
-     */
     const pageId = Object.keys(data.query.pages)[0];
     const images = data.query.pages[pageId]['images'];
     const urls = [];
-    let count =0;
+    let count = 0;
     for (let i = 0; i < images.length; i++) {
-      if ((images[i]['title']).includes(".jpg")) {
+      if ((images[i]['title']).includes('.jpg')) {
         let imagename = images[i]['title'];
         imagename = imagename.replace('Datei', 'File');
         urls[count] = `https://de.wikipedia.org/w/api.php?action=query&titles=${imagename}&prop=imageinfo&iilimit=50&iiend=2007-12-31T23:59:59Z&iiprop=url&format=json&formatversion=2&origin=*`;
@@ -127,9 +158,14 @@ async function wikiCall(longitude = 9.44376, latitude = 47.667223) {
     return urls;
   }
 
-  async function getImageUrl(urls) {
+  /**
+   * fetches the url and returns the content
+   * @param url
+   * @returns {Promise<*>}
+   */
+  async function getImageUrl(url) {
     let content;
-    await fetch(urls)
+    await fetch(url)
         .then((response) => response.json())
         .then((json) => content = json)
         .catch((response) => response.json());
