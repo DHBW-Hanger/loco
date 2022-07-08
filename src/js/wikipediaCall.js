918fc3f3746441f1124ac35b9661d29560cc0183
Andreas Holzapfel <holzapfel.a1994@gmail.com>
2022-07-08 19:48:19 +0200

    town, postcode and state are dynamically added, intelligent searchbar

--- src/js/wikipediaCall.js
+++ src/js/wikipediaCall.js
@@ -13,6 +13,17 @@ export async function reverseGeocoding(lon, lat) {
 }
 
 /**
+ *
+ * @param {string}search
+ * @returns {Promise<any>}
+ */
+export async function searchGeoCodeInfos(search){
+  const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${search}&format=json&polygon=1&addressdetails=1`);
+  const data = await response.json();
+  return data;
+}
+
+/**
  * returns the first extract in wikipedia about the location and removes all html elements and brackets
  * since the pageid changes from every apicall, the first key in the dictionary data['query']['pages] is collected
  *
@@ -24,9 +35,11 @@ export async function townInfoWiki(location) {
   let data = await getContent(url);
   try {
     data = data['query']['pages'];
+
     // gets first key from data
     const [id] = Object.keys(data);
     data = data[id]['extract'];
+    console.log(data);
     data = data.replace(/<[^>]+>/g, '');
     // removes everything between square brackets, however some wikipedia articles look bad then.
     data = data.replace(/ *\[[^\]]*]/g, '');
@@ -85,7 +98,7 @@ async function createImageAPIUrls(data) {
   const urls = [];
   let count = 0;
   for (let i = 0; i < images.length; i++) {
-    if ((images[i]['title']).includes('.jpg') && (count < 3)) {
+    if ((images[i]['title']).includes('.jpg') && (count < 1)) {
       let imagename = images[i]['title'];
       imagename = imagename.replace('Datei', 'File');
       urls[count] = `https://de.wikipedia.org/w/api.php?action=query&titles=${imagename}&prop=imageinfo&iilimit=50&iiend=2007-12-31T23:59:59Z&iiprop=url&format=json&formatversion=2&origin=*`;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ import '../css/modalsheet.css';
 import {BiLocationPlus} from 'react-icons/Bi';
 import {TiLocationArrowOutline} from 'react-icons/Ti';
 import {IoIosArrowUp, IoIosArrowDown} from 'react-icons/Io';
-import {getImages, townInfoWiki} from '../js/wikipediaCall';
+import {getImages, townInfoWiki, searchGeoCodeInfos} from '../js/wikipediaCall';
 
 
 import {
@@ -34,6 +34,9 @@ export default function App() {
   const [search, setSearch] = useState('');
   const [townInfo, setTownInfo] = useState({});
   const [townImage1, setTownImage1] = useState('');
+  const [town, setTown] = useState('');
+  const [state, setState] = useState('');
+  const [postcode, setPostcode] = useState('');
 
   //
   // muss noch mit Marker verbunden werden
@@ -51,19 +54,33 @@ export default function App() {
    */
   async function handleSearch(e) {
     if (search === '') return;
+    let geocodeInfo = await searchGeoCodeInfos(search);
 
-    // get the searched towns longitude and latitude
+    if (geocodeInfo.length === 0) {
+      console.log('empty');
+    } else {
+      // select top result
+      geocodeInfo = geocodeInfo[0];
 
+      let stadt = geocodeInfo.address.town;
+      setTown({town: stadt});
+      console.log(geocodeInfo.address);
 
-    // setTownInfo({town: await fetchDataWikiInfo(search) });
-    let towninfo = await townInfoWiki(search);
-    if (towninfo.length > 150) {
-      const wherecuttext = towninfo.indexOf(' ', 150);
-      towninfo = towninfo.substring(0, wherecuttext) + '...';
+      let towninfo = await townInfoWiki(stadt);
+      if (towninfo.length > 150) {
+        const wherecuttext = towninfo.indexOf(' ', 150);
+        towninfo = towninfo.substring(0, wherecuttext) + '...';
+      }
+      setTownInfo({townInfo: towninfo});
+      const images = await getImages(stadt);
+      setTownImage1({townImage1: images[0]});
+
+      // if country is Germany
+      if(geocodeInfo.address.country === "Deutschland"){
+        setState({state: geocodeInfo.address.state});
+        setPostcode({postcode: geocodeInfo.address.postcode});
+      }
     }
-    setTownInfo({townInfo: towninfo});
-    const images = await getImages(search);
-    setTownImage1({townImage1: images[0]});
   }
 
   const onPageBeforeOut = () => {
@@ -77,8 +94,8 @@ export default function App() {
     if (sheet.current) sheet.current.destroy();
   };
 
-  // Needs changes (on Button Press up and down the modal box)
-  // also add route infiormation if clikced on route (part close it and also make background appear as normal and nort darker)
+// Needs changes (on Button Press up and down the modal box)
+// also add route infiormation if clikced on route (part close it and also make background appear as normal and nort darker)
   /*eslint-disable*/
   const sheetPartClose = () => {
     // Close sheet part
@@ -92,109 +109,109 @@ export default function App() {
 
   /* eslint-enable*/
   return (
-    <Page name='home' onPageBeforeOut={onPageBeforeOut} onPageBeforeRemove={onPageBeforeRemove}>
+      <Page name='home' onPageBeforeOut={onPageBeforeOut} onPageBeforeRemove={onPageBeforeRemove}>
 
-      <Navbar>
-        <img className="logo" src="/icons/logo.svg" alt="Loco"/>
-        <div className="logo-text sliding">loco</div>
-        <Button fill sheetOpen=".demo-sheet-swipe-to-step">
-          Swipe To Step
-        </Button>
-        <NavRight>
-          <Link
-            searchbarEnable=".searchbar-demo"
-            iconIos="f7:search"
-            iconAurora="f7:search"
-            iconMd="material:search"
-          ></Link>
-        </NavRight>
-        <Searchbar
-          className="searchbar-demo"
-          expandable
-          searchContainer=".search-list"
-          searchIn=".item-title"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          disableButton={!theme.aurora}
-          onSubmit={handleSearch}
-        ></Searchbar>
-      </Navbar>
+        <Navbar>
+          <img className="logo" src="/icons/logo.svg" alt="Loco"/>
+          <div className="logo-text sliding">loco</div>
+          <Button fill sheetOpen=".demo-sheet-swipe-to-step">
+            Swipe To Step
+          </Button>
+          <NavRight>
+            <Link
+                searchbarEnable=".searchbar-demo"
+                iconIos="f7:search"
+                iconAurora="f7:search"
+                iconMd="material:search"
+            ></Link>
+          </NavRight>
+          <Searchbar
+              className="searchbar-demo"
+              expandable
+              searchContainer=".search-list"
+              searchIn=".item-title"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              disableButton={!theme.aurora}
+              onSubmit={handleSearch}
+          ></Searchbar>
+        </Navbar>
 
-      <MyMap/>
+        <MyMap/>
 
-      <Sheet
-        className="demo-sheet-swipe-to-step"
-        swipeToClose
-        swipeToStep
-        backdrop
-        onSheetStepClose={() => {
-          setSheetOpened(false);
-        }}
-        onSheetStepOpen={() => {
-          setSheetOpened(true);
-        }}
-      >
-        <div className="sheet-modal-swipe-step">
-          <div className="display-flex padding justify-content-space-between align-items-center">
+        <Sheet
+            className="demo-sheet-swipe-to-step"
+            swipeToClose
+            swipeToStep
+            backdrop
+            onSheetStepClose={() => {
+              setSheetOpened(false);
+            }}
+            onSheetStepOpen={() => {
+              setSheetOpened(true);
+            }}
+        >
+          <div className="sheet-modal-swipe-step">
+            <div className="display-flex padding justify-content-space-between align-items-center">
 
-            <div className="display-flex align-items-center">
-              <img
-                src={townImage1.townImage1}
-                alt="Avatar" className="wiki-pic"></img>
-              <div>
-                <b className="sheet-text-main">Friedrichshafen, 88540</b>
+              <div className="display-flex align-items-center">
+                <img
+                    src={townImage1.townImage1}
+                    alt="Avatar" className="wiki-pic"></img>
                 <div>
-                  <b className="sheet-text-secondary">{townInfo.townInfo}</b>
+                  <b className="sheet-text-main">{town.town}</b>
+                  <div>
+                    <b className="sheet-text-secondary">{townInfo.townInfo}</b>
+                  </div>
                 </div>
               </div>
+
+              <Button fill round align-items-center>
+                <div style={{fontSize: '24px', paddingTop: 7}}>
+                  <BiLocationPlus/>
+                </div>
+              </Button>
             </div>
 
-            <Button fill round align-items-center>
-              <div style={{fontSize: '24px', paddingTop: 7}}>
-                <BiLocationPlus/>
+            <div className="padding-horizontal padding-bottom">
+              <Button large fill round>
+                <div style={{fontSize: '24px', paddingTop: 4, paddingRight: 4}}>
+                  <TiLocationArrowOutline/>
+                </div>
+                Route
+              </Button>
+              <div className="margin-top text-align-center icon-color">
+                {sheetOpened === true ? <IoIosArrowDown/> : <IoIosArrowUp/>}
               </div>
-            </Button>
-          </div>
-
-          <div className="padding-horizontal padding-bottom">
-            <Button large fill round>
-              <div style={{fontSize: '24px', paddingTop: 4, paddingRight: 4}}>
-                <TiLocationArrowOutline/>
-              </div>
-              Route
-            </Button>
-            <div className="margin-top text-align-center icon-color">
-              {sheetOpened === true ? <IoIosArrowDown/> : <IoIosArrowUp/>}
             </div>
           </div>
-        </div>
 
-        <BlockTitle medium className="margin-top sheet-text-main">
+          <BlockTitle medium className="margin-top sheet-text-main">
             Information:
-        </BlockTitle>
+          </BlockTitle>
 
-        <List noHairlines className="sheet-container">
+          <List noHairlines className="sheet-container">
 
-          <ListItem title="Bundesland:" className="sheet-text-tertiary">
-            <b slot="after" className="sheet-text-tertiary-bold">
-                Baden-Württemberg
-            </b>
-          </ListItem>
+            <ListItem title="Bundesland:" className="sheet-text-tertiary">
+              <b slot="after" className="sheet-text-tertiary-bold">
+                {state.state}
+              </b>
+            </ListItem>
 
-          <ListItem title="Postleitzahlen:" className="sheet-text-tertiary">
-            <b slot="after" className="sheet-text-tertiary-bold">
-                88045, 88046, 88048
-            </b>
-          </ListItem>
+            <ListItem title="Postleitzahlen:" className="sheet-text-tertiary">
+              <b slot="after" className="sheet-text-tertiary-bold">
+                {postcode.postcode}
+              </b>
+            </ListItem>
 
-          <ListItem title="Einwohner:" className="sheet-text-tertiary">
-            <b slot="after" className="sheet-text-tertiary-bold">
+            <ListItem title="Einwohner:" className="sheet-text-tertiary">
+              <b slot="after" className="sheet-text-tertiary-bold">
                 61.221
-            </b>
-          </ListItem>
-        </List>
-      </Sheet>
+              </b>
+            </ListItem>
+          </List>
+        </Sheet>
 
-    </Page>
+      </Page>
   );
 }
