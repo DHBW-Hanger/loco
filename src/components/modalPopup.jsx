85a217a23c22df8d9e0e53ca4b0328db09f39d5f
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2022-07-10 18:13:20 +0200

    fixed linter warnings

--- src/components/modalPopup.jsx
+++ src/components/modalPopup.jsx
@@ -51,7 +51,8 @@ class ModalPopup extends Component {
                 <img src="/icons/logo.svg" alt="logo"/>
                 <p className="help-text"> Loco ist eine location-based progressive web app (PWA), welche dich bei deinen Reisen mit routing
                   services und grundlegenden Daten zu deinem Ziel unterstützt.<br/> <br/>
-                  Loco is a location-based progressive web app (PWA), which supports you in finding and providing basic information for your destination. <br/>
+                  Loco is a location-based progressive web app (PWA), which supports you in finding and providing basic information for your
+                  destination. <br/>
                   Loco also provides you this services offline, if you install it on your device before your offline usage.
                 </p>
               </div>
@@ -59,8 +60,9 @@ class ModalPopup extends Component {
                 <p className="help-text"> Mit der Stecknadel signalisierst unseren routing services dein Ziel,
                   verschiebe sie einfach dort wo du hin willst und die optimale Route wird für dich auf der Karte angezeigt.
                   Falsches Ziel? Kein Problem, einfach nochmal verschieben!
-                  <br/> <br/>
-                  By moving the pin where you want to go you're signaling our routing services your destination, so move it and the optimal route will be displayed for you.
+                <br/> <br/>
+                  By moving the pin where you want to go you&apos;re signaling our routing services your destination, so move it and the optimal route
+                  will be displayed for you.
                   Wrong location? No problem at all, just move it again!
                 </p>
                 <img src="/icons/marker_darkm.svg" alt="logo"/>
@@ -70,16 +72,17 @@ class ModalPopup extends Component {
                 <p className="help-text"> Der Recenter Button ist für dich da, wenn du nach deiner Suche auf der Karte
                   wieder auf deinen Standort und deine Route zurück willst. Der Button wird allerdings nur angezeigt,
                   wenn dein Standort nicht in der Mitte der Karte ist.
-                  <br/> <br/>
-                  The recenter button is there for you if, after your search on the map, you  want to go back to your
-                  location and the route towards your destination. However, the recenter button will only appear when you're not centered. So no need to worry!
+                <br/> <br/>
+                  The recenter button is there for you if, after your search on the map, you want to go back to your
+                  location and the route towards your destination. However, the recenter button will only appear when you&apos;re not centered. So no
+                  need to worry!
                 </p>
               </div>
               <div className="help-section">
                 <p className="help-text"> Mit dem Help Button wird dieses Popup geöffnet, falls du von den Controls etwas nachschauen möchtest.
                   <br/>
                   <br/>
-                  By clicking the help button, this pupup will be opened, if at any point in time you're lost again.
+                  By clicking the help button, this pupup will be opened, if at any point in time you&apos;re lost again.
                 </p>
                 <img src="/icons/help.svg" alt="logo"/>
               </div>
@@ -93,7 +96,7 @@ class ModalPopup extends Component {
           </Modal.Body>
         </Modal>
       </div>
-    )
+    );
   }
 }
 
