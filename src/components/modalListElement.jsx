23196f4d74668d7a2d191033421062712bf79504
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2022-07-13 10:25:58 +0200

    added modal list element

---
+++ src/components/modalListElement.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {ListItem} from "framework7-react";
+
+
+/**
+ * modalListElement component
+ * @param {string} data - data to be displayed
+ * @param {string} title - title of the element
+ */
+class ModalListElement extends Component {
+  /**
+   * @param {object} props
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.date,
+      title: props.title,
+    };
+  }
+
+  /**
+   * render the component
+   * @return {JSX.Element}
+   */
+  render() {
+    return (
+      (this.props.data) ?
+        <ListItem title={`${this.props.title}:`} className="sheet-text-tertiary">
+          <b slot="after" className="sheet-text-tertiary-bold">
+            {this.props.date}
+          </b>
+        </ListItem> : ''
+    );
+  }
+}
+
+export default ModalListElement;
