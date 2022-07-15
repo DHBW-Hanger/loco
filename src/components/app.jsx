65f2638ec95f3e5cbd10d5a504913db6b5c4d99d
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2022-07-15 19:18:46 +0200

    removed unused files

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,41 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import '../css/index.css';
 
 import {
-  f7,
-  f7ready,
   App,
   Views,
   View,
-  Popup,
-  Page,
-  Navbar,
-  NavRight,
-  Link,
-  Block,
-  LoginScreen,
-  LoginScreenTitle,
-  List,
-  ListInput,
-  ListButton,
-  BlockFooter,
 } from 'framework7-react';
 
 
 import routes from '../js/routes';
-import store from '../js/store';
 
 const MyApp = () => {
-  // Login screen demo data
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
   // Framework7 Parameters
   const f7params = {
     name: 'Loco', // App name
     theme: 'auto', // Automatic theme detection
-    // App store
-    store: store,
     // App routes
     routes: routes,
     // Register service worker (only on production build)
@@ -43,14 +23,6 @@ const MyApp = () => {
       path: '/service-worker.js',
     } : {},
   };
-  const alertLoginData = () => {
-    f7.dialog.alert('Username: ' + username + '<br>Password: ' + password, () => {
-      f7.loginScreen.close();
-    });
-  };
-  f7ready(() => {
-    // Call F7 APIs here
-  });
 
   return (
     <App {...f7params}>
@@ -59,52 +31,6 @@ const MyApp = () => {
         {/* Your main view/tab, should have "view-main" class. It also has "tabActive" prop */}
         <View id="view-home" main tab tabActive url="/"/>
       </Views>
-
-      {/* Popup */}
-      <Popup id="my-popup">
-        <View>
-          <Page>
-            <Navbar title="Popup">
-              <NavRight>
-                <Link popupClose>Close</Link>
-              </NavRight>
-            </Navbar>
-            <Block>
-              <p>Popup content goes here.</p>
-            </Block>
-          </Page>
-        </View>
-      </Popup>
-
-      <LoginScreen id="my-login-screen">
-        <View>
-          <Page loginScreen>
-            <LoginScreenTitle>Login</LoginScreenTitle>
-            <List form>
-              <ListInput
-                type="text"
-                name="username"
-                placeholder="Your username"
-                value={username}
-                onInput={(e) => setUsername(e.target.value)}
-              />
-              <ListInput
-                type="password"
-                name="password"
-                placeholder="Your password"
-                value={password}
-                onInput={(e) => setPassword(e.target.value)}
-              />
-            </List>
-            <List>
-              <ListButton title="Sign In" onClick={() => alertLoginData()}/>
-              <BlockFooter>
-                Some text about login information.<br/>Click &quot;Sign In&quot; to close Login Screen
-              </BlockFooter>
-            </List>
-          </Page>
-        </View>
-      </LoginScreen>
     </App>
   );
 };
