c6c71a22deb06d2a5cb4cb49deca98e657ee8bc3
Tobias-Jung-DHBW <jung.tobias-it21@it.dhbw-ravensburg.de>
2022-07-17 19:35:02 +0200

    Updated Beta

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,16 +1,2 @@
-if (!self.define) {
-  let e; const s={}; const a=(a, r)=>(a=new URL(a+'.js', r).href, s[a]||new Promise(((s)=>{
-    if ('document'in self) {
-      const e=document.createElement('script'); e.src=a, e.onload=s, document.head.appendChild(e);
-    } else e=a, importScripts(a), s();
-  })).then((()=>{
-    const e=s[a]; if (!e) throw new Error(`Module ${a} didn’t register its module`); return e;
-  }))); self.define=(r, i)=>{
-    const f=e||('document'in self?document.currentScript.src:'')||location.href; if (s[f]) return; const n={}; const o=(e)=>a(e, f); const d={module: {uri: f}, exports: n, require: o}; s[f]=Promise.all(r.map(((e)=>d[e]||o(e)))).then(((e)=>(i(...e), n)));
-  };
-}define(['./workbox-7d6a3f4d'], (function(e) {
-  'use strict'; self.addEventListener('message', ((e)=>{
-    e.data&&'SKIP_WAITING'===e.data.type&&self.skipWaiting();
-  })), e.precacheAndRoute([{url: 'assets/apple-touch-icon-180x180.a8a626e7.png', revision: 'ddcec43383ea7c5e82452998573573fa'}, {url: 'assets/favicon-196x196.5d69e089.png', revision: '314d13b36b4119fda98a6d7f5cd5d933'}, {url: 'assets/Framework7Icons-Regular.a42aa071.woff2', revision: '4a39aba9fb8a2f831fa437780e1a058a'}, {url: 'assets/Framework7Icons-Regular.eba1e821.woff', revision: 'd03b787b6492fa2b0141c43fb7e56689'}, {url: 'assets/index.0de8b8ea.js', revision: 'cd4956ff768bd2f4d29e927d5d9d05bc'}, {url: 'assets/index.8ca32f33.css', revision: 'f481e6234c1384a260b5a51e15845c58'}, {url: 'assets/layers-2x.066daca8.png', revision: '4f0283c6ce28e888000e978e537a6a56'}, {url: 'assets/layers.1dbbe9d0.png', revision: 'a6137456ed160d7606981aa57c559898'}, {url: 'assets/marker-icon.574c3a5c.png', revision: '2273e3d8ad9264b7daa5bdbf8e6b47f8'}, {url: 'assets/material-icons.812f3b69.woff', revision: '54dfa9d2d4cb77b80750249adc5b2d1e'}, {url: 'assets/material-icons.f082f7fa.woff2', revision: '9f5d66557035535842ac29f97e57fe62'}, {url: 'index.html', revision: '0edb0fbf3106ce6a89b27742280acebf'}], {ignoreURLParametersMatching: [/^utm_/, /^fbclid$/]});
-}));
-// # sourceMappingURL=service-worker.js.map
+if(!self.define){let e,s={};const a=(a,i)=>(a=new URL(a+".js",i).href,s[a]||new Promise((s=>{if("document"in self){const e=document.createElement("script");e.src=a,e.onload=s,document.head.appendChild(e)}else e=a,importScripts(a),s()})).then((()=>{let e=s[a];if(!e)throw new Error(`Module ${a} didn’t register its module`);return e})));self.define=(i,r)=>{const o=e||("document"in self?document.currentScript.src:"")||location.href;if(s[o])return;let d={};const n=e=>a(e,o),f={module:{uri:o},exports:d,require:n};s[o]=Promise.all(i.map((e=>f[e]||n(e)))).then((e=>(r(...e),d)))}}define(["./workbox-7d6a3f4d"],(function(e){"use strict";self.addEventListener("message",(e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()})),e.precacheAndRoute([{url:"assets/apple-touch-icon-180x180.a8a626e7.png",revision:"ddcec43383ea7c5e82452998573573fa"},{url:"assets/favicon-196x196.5d69e089.png",revision:"314d13b36b4119fda98a6d7f5cd5d933"},{url:"assets/Framework7Icons-Regular.a42aa071.woff2",revision:"4a39aba9fb8a2f831fa437780e1a058a"},{url:"assets/Framework7Icons-Regular.eba1e821.woff",revision:"d03b787b6492fa2b0141c43fb7e56689"},{url:"assets/index.085103e7.js",revision:"aa101217ae3bf723dba5a6a60b9e8969"},{url:"assets/index.c3575887.css",revision:"6d241b578124d5915ebd22f08ae81ac7"},{url:"assets/mapbox-gl.48d87c0d.js",revision:"f3c64e94331e09d296ff5bcf4f99619f"},{url:"assets/material-icons.18d2477b.woff2",revision:"2e2b1be32d322f19f8ead86bd148cd72"},{url:"assets/material-icons.66dca61a.woff",revision:"7814880c701adbefed31b0647a92570c"},{url:"index.html",revision:"4e1be7cbd20bae5abfaadd0ab2e56765"}],{ignoreURLParametersMatching:[/^utm_/,/^fbclid$/]})}));
+//# sourceMappingURL=service-worker.js.map
