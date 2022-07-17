99db49a05a37b219a09ec795d8b38e36aa6385dd
Tobias-Jung-DHBW <jung.tobias-it21@it.dhbw-ravensburg.de>
2022-07-17 14:27:39 +0200

    Updated modalsheet

--- src/components/modalSheet.jsx
+++ src/components/modalSheet.jsx
@@ -1,20 +1,15 @@
 import React, {Component} from 'react';
-import {BiLocationPlus} from 'react-icons/Bi';
 import {TiLocationArrowOutline} from 'react-icons/Ti';
 import {IoIosArrowDown, IoIosArrowUp} from 'react-icons/Io';
-import {geocodeTown} from '../js/wikipediaCall';
-import '../css/modalsheet.css';
+import '../css/modalSheet.css';
 
 import {
   Button,
   Sheet,
   BlockTitle,
   List,
-  ListItem,
 } from 'framework7-react';
-
-
-// https://v4.framework7.io/react/sheet-modal
+import ModalListElement from './modalListElement';
 
 
 /**
@@ -35,13 +30,17 @@ class ModalSheet extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      completeAddress: props.completeAddress,
       townName: props.townName,
       townDescription: props.townDescription,
+      townDescription2: props.townDescription2,
       townImage: props.townImage,
       federalState: props.federalState,
       postCode: props.postCode,
       population: props.population,
       targetMarkerLocation: props.targetMarkerLocation,
+      sheetOpened: true,
+      showMoreInfo: false,
     };
   }
 
@@ -56,86 +55,69 @@ class ModalSheet extends Component {
         swipeToClose
         swipeToStep
         backdrop
+        onSheetStepClose={() => {
+          this.setState({sheetOpened: !this.state.sheetOpened});
+        }}
+        onSheetStepOpen={() => {
+          this.setState({sheetOpened: !this.state.sheetOpened});
+        }}
       >
         <div className="sheet-modal-swipe-step">
-          <div className="display-flex padding justify-content-space-between align-items-center">
+          <div className="sheet-header display-flex padding justify-content-left align-items-center">
             <img src={this.props.townImage}
               alt="city image" className="wiki-pic"/>
             <div className="display-flex align-items-center">
-              <div>
+              <div className="city-text">
                 <b className="sheet-text-main">{this.props.townName}</b>
                 <div>
-                  <b className="sheet-text-secondary">{this.props.townDescription}</b>
+                  <b className="sheet-text-secondary">{this.props.townDescription} </b>
+                  {(this.state.showMoreInfo === true) ?
+                    <b className="sheet-text-secondary">{this.props.townDescription2}</b> : ''}
+                  {(this.props.townDescription2) ?
+                    <Button className="button-more-description" onClick={() => {
+                      this.setState({showMoreInfo: !this.state.showMoreInfo});
+                    }}>
+                      {this.state.showMoreInfo === true ? 'Hide' : 'More'}
+                    </Button> : ''}
                 </div>
               </div>
             </div>
-
-            <Button fill round align-items-center onClick={
-              () => {
-                geocodeTown(this.props.townName).then((response) => {
-                  const lat = response[0].lat;
-                  const lon = response[0].lon;
-                  this.setState({
-                    targetMarkerLocation: {lat, lon},
-                  }, () => {
-                    console.log(this.state.targetMarkerLocation);
-                  });
-                });
-              }
-            }>
-              <div style={{fontSize: '24px', paddingTop: 7}}>
-                <BiLocationPlus/>
-              </div>
-            </Button>
           </div>
 
           <div className="padding-horizontal padding-bottom">
-            <Button large fill round>
+            <Button large fill round onClick={() => {
+              console.log(this.props.targetMarkerLocation);
+              const event = new CustomEvent('updateLocation', {
+                detail: {
+                  lat: this.props.targetMarkerLocation.lat,
+                  lng: this.props.targetMarkerLocation.lng,
+                }});
+              window.dispatchEvent(event);
+            }}>
               <div style={{fontSize: '24px', paddingTop: 4, paddingRight: 4}}>
                 <TiLocationArrowOutline/>
               </div>
               Route
             </Button>
             <div className="margin-top text-align-center icon-color">
-              {/* TODO turn arrow on step */}
-              {true ? <IoIosArrowDown/> : <IoIosArrowUp/>}
+              {(this.state.sheetOpened === false) ?
+                <IoIosArrowDown/> : <IoIosArrowUp/>}
             </div>
           </div>
         </div>
-
         <BlockTitle medium className="margin-top sheet-text-main">
           Information:
         </BlockTitle>
 
         <List noHairlines className="sheet-container">
-
-          <ListItem title="Land:" className="sheet-text-tertiary">
-            <b slot="after" className="sheet-text-tertiary-bold">
-              {this.props.countryName}
-            </b>
-          </ListItem>
-
-          <ListItem title="Bundesland:" className="sheet-text-tertiary">
-            <b slot="after" className="sheet-text-tertiary-bold">
-              {this.props.federalState}
-            </b>
-          </ListItem>
-
-          <ListItem title="Postleitzahlen:" className="sheet-text-tertiary">
-            <b slot="after" className="sheet-text-tertiary-bold">
-              {this.props.postCode}
-            </b>
-          </ListItem>
-
-          <ListItem title="Einwohner:" className="sheet-text-tertiary">
-            <b slot="after" className="sheet-text-tertiary-bold">
-              {this.props.population}
-            </b>
-          </ListItem>
+          <ModalListElement title="Adresse" data={this.props.completeAddress}/>
+          <ModalListElement title="Land" data={this.props.countryName}/>
+          <ModalListElement title="Bundesland" data={this.props.federalState}/>
+          <ModalListElement title="Postleitzahlen" data={this.props.postCode}/>
+          <ModalListElement title="Einwohner" data={this.props.population}/>
         </List>
       </Sheet>
     );
   }
 }
-
 export default ModalSheet;
